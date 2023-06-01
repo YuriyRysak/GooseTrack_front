c5861c2e230661fcbfb7761f6a4f7b7463bbaced
Oleh Kliapko <goldclub1977@gmail.com>
2023-06-01 13:48:41 +0300

    add libs

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,17 @@
+import { useSelector } from 'react-redux';
+// import {
+//   getIsLoggedIn,
+//   getUser,
+//   getIsRefreshingUser,
+// } from 'redux/auth/authSelectors';
+
+export const useAuth = () => {
+  //   const isLoggedIn = useSelector(getIsLoggedIn);
+  //   const isRefreshing = useSelector(getIsRefreshingUser);
+  //   const user = useSelector(getUser);
+  //   return {
+  //     isLoggedIn,
+  //     isRefreshing,
+  //     user,
+  //   };
+};
