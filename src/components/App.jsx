0f5c134e2eae482d4fabba2c72ce561140481009
Oleh Kliapko <goldclub1977@gmail.com>
2023-06-01 15:23:57 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+
+import MainLayout from '../components/User/MainLayout';
+
+const MainPage = lazy(() => import('../Pages/MainPage'));
+const AccountPage = lazy(() => import('../Pages/AccountPage'));
+const CalendarPage = lazy(() => import('../Pages/CalendarPage'));
+const LoginPage = lazy(() => import('../Pages/LoginPage'));
+const RegisterPage = lazy(() => import('../Pages/RegisterPage'));
+const NotFoundPagePage = lazy(() => import('../Pages/NotFoundPagePage'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />}>
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="calendar" element={<MainLayout />}>
+            <Route path="account" element={<AccountPage />} />
+            <Route index element={<CalendarPage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPagePage />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
