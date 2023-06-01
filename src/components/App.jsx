a5cbcb6bd6d3266f35f2a96190651760d6934883
Oleh Kliapko <goldclub1977@gmail.com>
2023-06-01 18:04:55 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,26 +3,25 @@ import { lazy } from 'react';
 
 import MainLayout from '../components/User/MainLayout/MainLayout';
 
-const MainPage = lazy(() => import('../pages/MainPage'));
-const AccountPage = lazy(() => import('../pages/AccountPage'));
-const CalendarPage = lazy(() => import('../pages/CalendarPage'));
-const LoginPage = lazy(() => import('../pages/LoginPage'));
-const RegisterPage = lazy(() => import('../pages/RegisterPage'));
-const NotFoundPagePage = lazy(() => import('../pages/NotFoundPage'));
+const MainPage = lazy(() => import('pages/MainPage'));
+const AccountPage = lazy(() => import('pages/AccountPage'));
+const CalendarPage = lazy(() => import('pages/CalendarPage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
+const NotFoundPagePage = lazy(() => import('pages/NotFoundPage'));
 
 export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainPage />}>
-          <Route path="register" element={<RegisterPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="calendar" element={<MainLayout />}>
-            <Route path="account" element={<AccountPage />} />
-            <Route index element={<CalendarPage />} />
-          </Route>
-          <Route path="*" element={<NotFoundPagePage />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="calendar" element={<MainLayout />}>
+          <Route path="account" element={<AccountPage />} />
+          <Route index element={<CalendarPage />} />
         </Route>
+        <Route path="*" element={<NotFoundPagePage />} />
       </Routes>
     </>
   );
