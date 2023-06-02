422e6ed1ba97e86be57bee9b13a6f1bc7c5db928
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-02 11:06:08 +0300

    delete extra brackets

--- src/utils/Loader/Loader.jsx
+++ src/utils/Loader/Loader.jsx
@@ -4,13 +4,11 @@ import { themes } from "styles/themes";
 
 export const Loader = () => {
     return(
-        <>
-            <Wrapper>
-                <ColorRing 
-                    colors={themes.colors.loaderCircle}
-                />
-            </Wrapper>
-        </>
+        <Wrapper>
+            <ColorRing 
+                colors={themes.colors.loaderCircle}
+            />
+        </Wrapper>
     )
 };
 
