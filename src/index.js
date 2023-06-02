145e8751925ade9d8e10ff958f6a83d767a6e7f8
maxyma-sanx <maxymasan.dev@gmail.com>
2023-06-02 11:51:10 +0300

    add ThemeProvider

--- src/index.js
+++ src/index.js
@@ -8,13 +8,17 @@ import {
 } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
+import { ThemeProvider } from '@emotion/react';
+import { themes } from 'styles/themes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
       <BrowserRouter basename="GooseTrack_front">
-        <App />
+        <ThemeProvider theme={themes}>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
       {/* </PersistGate> */}
     </Provider>
