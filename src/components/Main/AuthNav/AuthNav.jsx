28e842945887d67531c38d5b6deb2b9ed61b22e2
maxyma-sanx <maxymasan.dev@gmail.com>
2023-06-03 20:06:04 +0300

    add authNav

--- src/components/Main/AuthNav/AuthNav.jsx
+++ src/components/Main/AuthNav/AuthNav.jsx
@@ -1,7 +1,15 @@
+import { CgLogIn } from 'react-icons/cg';
+import { AuthLink } from 'utils/Buttons/MainButton.styled';
+import { Wrapper } from './AuthNav.styled';
+
 export const AuthNav = () => {
   return (
-    <div>
-      <h3>Auth Nav</h3>
-    </div>
+    <Wrapper>
+      <AuthLink to={'/login'}>Sign up</AuthLink>
+      <AuthLink to={'/register'} color="blue" colorbtn="white">
+        Log in
+        <CgLogIn style={{ marginLeft: 6, width: 18, height: 18 }} />
+      </AuthLink>
+    </Wrapper>
   );
 };
