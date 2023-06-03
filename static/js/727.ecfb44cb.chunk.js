47e3d71a241f9809e64a9e84c87bb6a94b7538dc
Oleh-Kliapko <goldclub1977@gmail.com>
2023-06-03 12:40:00 +0000

    Deploying to gh-pages from @ Oleh-Kliapko/GooseTrack_front@380822555c187aef2ca1ced6c8b24787082542c1 🚀

---
+++ static/js/727.ecfb44cb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkGooseTrack_front=self.webpackChunkGooseTrack_front||[]).push([[727],{727:function(s,e,n){n.r(e);var r=n(4895),c=n(184);e.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)(r.h4,{}),(0,c.jsx)(r.dk,{}),(0,c.jsx)(r.IO,{})]})}}}]);
+//# sourceMappingURL=727.ecfb44cb.chunk.js.map
