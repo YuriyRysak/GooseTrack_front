a0eaf243762a4feceb7b9d8828c0e210d7312a33
Oleh Kliapko <goldclub1977@gmail.com>
2023-06-03 22:55:48 +0300

    Create CalendarPage.jsx

---
+++ src/pages/CalendarPage.jsx
@@ -0,0 +1,13 @@
+import { TasksColumn, TasksColumnsList } from 'components/User';
+
+const CalendarPage = () => {
+  return (
+    <div>
+      <h3>CalendarPage</h3>
+      <TasksColumn />
+      <TasksColumnsList />
+    </div>
+  );
+};
+
+export default CalendarPage;
