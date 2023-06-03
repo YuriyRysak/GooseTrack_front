5ca6c831c47e0e281442a183d0ed71fbaa424a8b
maxyma-sanx <maxymasan.dev@gmail.com>
2023-06-03 20:19:25 +0300

    authNav breakpoints

--- src/components/Main/AuthNav/AuthNav.styled.js
+++ src/components/Main/AuthNav/AuthNav.styled.js
@@ -2,5 +2,13 @@ import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
   display: flex;
-  gap: 10px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 200px;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.m}) {
+    flex-direction: row;
+    gap: 10px;
+  }
 `;
