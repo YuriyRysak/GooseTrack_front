23ffb3f5150e16dc1a3cc2a157d6481ce00a14f4
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-04 10:07:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,20 +4,29 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-import { store, persistor } from 'redux/store';
-import './index.css';
 import { ThemeContextProvider } from 'components/User/Header/ThemeToggler/ThemeContext';
+import {
+  store,
+  persistor
+} from 'redux/store';
+import './index.css';
+import { NotificationProvider } from 'helpers';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="GooseTrack_front">
           <ThemeContextProvider>
-            <App />
-          </ThemeContextProvider>
-        </BrowserRouter>
-      </PersistGate>
+            <NotificationProvider>
+                <App />
+              </NotificationProvider>
+            </ThemeContextProvider>
+          </BrowserRouter>
+       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
+
+
+
