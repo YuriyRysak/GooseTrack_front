90562609e097eed6fa6f5f03cfca505b81c21024
Yulia <nayanzina@gmail.com>
2023-06-04 22:11:24 +0300

    header created

--- src/components/User/Header/Header.jsx
+++ src/components/User/Header/Header.jsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { UserInfo, ThemeToggler, AddFeedbackModal } from '../Header';
+import { HeaderWrap, BtnAddFeedback, HeaderTitle, MenuBtn, UserInfoTest } from './Header.styled';
+import { ReactComponent as BurgerMenu } from '../../../images/svg/burger.svg';
 
 export const Header = () => {
+  let locationPath = useLocation().pathname;
+  const [isAccPage, setisAccPage] = useState(false);
+  
+  useEffect(() => {
+    if (locationPath.includes('account')) {
+      setisAccPage(true);
+    } else {setisAccPage(false);}
+  }, [locationPath]);
+
   return (
-    <div>
-      <h3>Header</h3>
-      <UserInfo />
+    <HeaderWrap>
+      <HeaderTitle>{isAccPage ? 'User Profile' : 'Calendar' }</HeaderTitle>
+      <MenuBtn><BurgerMenu /></MenuBtn>
+      {!isAccPage && <BtnAddFeedback type='button'>Feedback</BtnAddFeedback>}
       <ThemeToggler />
-      <AddFeedbackModal />
-    </div>
+      {/* <AddFeedbackModal /> */}
+      <UserInfoTest>User Info</UserInfoTest>
+       {/* <UserInfo /> */}
+    </HeaderWrap>
   );
 };
