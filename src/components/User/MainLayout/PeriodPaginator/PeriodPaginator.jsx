33c9180dbc6d8390b8078ca0741b56ee116c32a9
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-06-06 00:13:08 +0300

    btn periodPaginator

--- src/components/User/MainLayout/PeriodPaginator/PeriodPaginator.jsx
+++ src/components/User/MainLayout/PeriodPaginator/PeriodPaginator.jsx
@@ -1,7 +1,14 @@
-export const PeriodPaginator = (data, type, changeDate) => {
+import { IconPag, PeriodBtn } from 'utils/Buttons/MainButton.styled';
+
+export const PeriodPaginator = ({ data, type, changeDate }) => {
   return (
-    <div>
-      <h3>Period Paginator</h3>
-    </div>
+    <>
+      <PeriodBtn onClick={console.log('left')}>
+        <IconPag id="left" />
+      </PeriodBtn>
+      <PeriodBtn onClick={console.log('right')} id="right">
+        <IconPag />
+      </PeriodBtn>
+    </>
   );
 };
