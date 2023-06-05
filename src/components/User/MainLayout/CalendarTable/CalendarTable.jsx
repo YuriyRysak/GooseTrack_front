413193242c613263fe264df29880c19ceaac0330
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-05 19:31:55 +0300

    delete unneccessary variables

--- src/components/User/MainLayout/CalendarTable/CalendarTable.jsx
+++ src/components/User/MainLayout/CalendarTable/CalendarTable.jsx
@@ -5,8 +5,8 @@ import { CalendarTableContainer, Week } from "./CalendarTable.styled";
 export const CalendarTable = () => {
 
   const currentDate = '2023-06-04';
-  const amounOfDays = getMonthDetails(currentDate).amountOfDays;
-  const amountOfWeeks = getMonthDetails(currentDate).amountOfWeeks;
+  // const amounOfDays = getMonthDetails(currentDate).amountOfDays;
+  // const amountOfWeeks = getMonthDetails(currentDate).amountOfWeeks;
   const numberOffirstWeek = getMonthDetails(currentDate).numberOfFirstWeek;
   const numberOfLastWeek = getMonthDetails(currentDate).numberOfLastWeek;
   const getWeeksArray = () => {
@@ -57,7 +57,7 @@ export const CalendarTable = () => {
     title: "Lear...",
     priority: "low"
 }]
-  const daysArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+  // const daysArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
   const oneWeekArray = [1, 2, 3, 4, 5, 6, 7];
 
   return (
